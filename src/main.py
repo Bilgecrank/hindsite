2ec17f3f631b6646aa083fe5fb587cb1134a9203
Vincent Testagrossa <lancevancevt@gmail.com>
2024-02-05 10:34:56 -0500

    fixed some linting issues

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+'''
+Test
+'''
+import os
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-
-import os
 
 # Included to load .env environment variables for local dev
 load_dotenv()
@@ -15,7 +18,7 @@ database_uri = ("mysql+pymysql://"
                 + os.environ['MYSQLUSER'] + ":"
                 + os.environ['MYSQL_ROOT_PASSWORD'] + "@"
                 + os.environ['MYSQLHOST'] + ":"
-                + os.environ['MYSQLPORT'] + "/" 
+                + os.environ['MYSQLPORT'] + "/"
                 + os.environ['MYSQL_DATABASE'])
 
 # Needed to redirect default paths to maintain the proposed folder structure
@@ -30,6 +33,9 @@ app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 class Users(db.Model):
+    '''
+        Sets up the database model
+    '''
     __tablename__ = 'users'
     id = db.Column('user_id', db.Integer, primary_key = True)
     name = db.Column(db.String(100))
@@ -39,9 +45,12 @@ def __init__(self, name):
 
 @app.route('/')
 def index():
+    '''
+        Loads index.html, sets the title and users
+    '''
     title = 'Deployment Test'
     return render_template('index.html', title=title, users = Users.query.all())
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True, port=os.getenv("PORT", default=80))
+    app.run(debug=True, port=os.getenv("PORT", default="80"))
