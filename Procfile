3410d571688fbbfb05e1ac03989775970146a88b
Vincent Testagrossa <lancevancevt@gmail.com>
2024-02-05 10:23:50 -0500

    added comments and changed paths for templates/static

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --timeout 600 --chdir src/app main:app
+web: gunicorn --timeout 600 --chdir src main:app
--- src/main.py
+++ src/main.py
@@ -4,8 +4,13 @@ from dotenv import load_dotenv
 
 import os
 
+# Included to load .env environment variables for local dev
 load_dotenv()
 
+# Location agnostic URI that loads either the .env file, or if
+# it's not included loads the variables set by the OS. In the
+# case of Railway.app, the variables set by the database and
+# linked to the app.
 database_uri = ("mysql+pymysql://"
                 + os.environ['MYSQLUSER'] + ":"
                 + os.environ['MYSQL_ROOT_PASSWORD'] + "@"
@@ -13,9 +18,14 @@ database_uri = ("mysql+pymysql://"
                 + os.environ['MYSQLPORT'] + "/" 
                 + os.environ['MYSQL_DATABASE'])
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+# Needed to redirect default paths to maintain the proposed folder structure
+# since Flask looks for static and templates in the root folder of the app
+template_dir = os.path.abspath('app/templates')
+static_dir = os.path.abspath('app/static')
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
+# Sets the database URI to match whatever environment it's in
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 
 db = SQLAlchemy(app)
