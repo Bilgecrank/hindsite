b2f774d0723aefe29f9e3ef45f095bdb9f0a3ec2
Vincent Testagrossa <lancevancevt@gmail.com>
2024-02-07 14:02:39 -0500

    offcanvas functions with breakpoints for large and small screens

--- src/main.py
+++ src/main.py
@@ -18,8 +18,40 @@ def index():
     '''
         Loads index.html, sets the title
     '''
-    title = 'Deployment Test'
+    title = 'Index'
     return render_template('index.html', title=title)
+
+@app.route('/retrospective')
+def retrospective():
+    '''
+        Loads retrospective.html, sets the title
+    '''
+    title = 'Retrospective'
+    return render_template('retrospective.html', title=title)
+
+@app.route('/history')
+def history():
+    '''
+        Loads history.html, sets the title
+    '''
+    title = 'History'
+    return render_template('history.html', title=title)
+
+@app.route('/group')
+def group():
+    '''
+        Loads group.html, sets the title
+    '''
+    title = 'Group'
+    return render_template('group.html', title=title)
+
+@app.route('/settings')
+def settings():
+    '''
+        Loads settings.html, sets the title
+    '''
+    title = 'Settings'
+    return render_template('settings.html', title=title)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default="80"))
