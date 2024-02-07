6e0300166f9bd4cf3ec7b87cd4d94d940240e207
Bilgecrank <8432868+Bilgecrank@users.noreply.github.com>
2024-02-07 12:19:31 -0800

    Adjusted main file to dynamically add/remove tables.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-'''
+"""
 Test
-'''
+"""
 import os
 
 from flask import Flask, render_template
@@ -23,8 +23,8 @@ database_uri = ("mysql+pymysql://"
 
 # Needed to redirect default paths to maintain the proposed folder structure
 # since Flask looks for static and templates in the root folder of the app
-template_dir = os.path.abspath('app/templates')
-static_dir = os.path.abspath('app/static')
+template_dir = os.path.abspath('src/app/templates')
+static_dir = os.path.abspath('src/app/static')
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 # Sets the database URI to match whatever environment it's in
@@ -32,25 +32,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
 class Users(db.Model):
-    '''
-        Sets up the database model
-    '''
+    """
+    Sets up the database model
+    """
     __tablename__ = 'users'
     id = db.Column('user_id', db.Integer, primary_key = True)
     name = db.Column(db.String(100))
+    def __init__(self, name):
+        self.name = name
 
-def __init__(self, name):
-    self.name = name
 
 @app.route('/')
 def index():
-    '''
-        Loads index.html, sets the title and users
-    '''
+    """
+    Loads index.html, sets the title and users
+    """
     title = 'Deployment Test'
-    return render_template('index.html', title=title, users = Users.query.all())
+    return render_template('index.html', title=title, users=Users.query.all())
+
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    ollanius = Users('Ollanius Pius')
+    fabius = Users('Fabius Bile')
+    db.session.add(ollanius)
+    db.session.add(fabius)
+    db.session.commit()
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True, port=os.getenv("PORT", default="80"))
