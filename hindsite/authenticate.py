3a0c58f98b6b4b5fbd35dc93fcadcfe3d7255ba9
Bilgecrank <8432868+Bilgecrank@users.noreply.github.com>
2024-02-09 15:19:03 -0800

    Login logic tested, Registeration logic not yet tested.

--- hindsite/authenticate.py
+++ hindsite/authenticate.py
@@ -4,9 +4,19 @@ Defines logic flow for the authentication process.
 import re  # For serverside validation of secrets.
 
 import bcrypt
-
-from hindsite.db_setup import db
 import hindsite.sql_query as query
+
+
+class RegisterError(Exception):
+    """
+    Definition for errors raised by the register_user function
+    """
+
+
+class LoginError(Exception):
+    """
+    Definition for errors raised by the login function
+    """
 
 
 def register_user(email: str, password: str):
@@ -17,15 +27,19 @@ def register_user(email: str, password: str):
     :param email:
     :param password:
     :return: **str** Returns a string indicating an error, or None if there is no error.
+
+    :raises RegisterError: Raises this in case of an already extant account or if the password is
+     not a valid secret.
     """
-    if not query.is_user(email):
-        return 'ERROR: An account already exists with this email.'
+    if query.is_user(email):
+        raise RegisterError('ERROR: An account already exists with this email.')
     if not valid_secret(password):
-        return ("Passwords must at least 12 characters long, include 1 uppercase letter, 1 lowercase letter, 1 number, "
-                "and 1 special character (!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~).")
-    hashword = bcrypt.hashpw(password.encode('utf-8'))
+        raise RegisterError(
+            'Passwords must at least 12 characters long, include 1 uppercase letter, 1 lowercase '
+            'letter, 1 number, and 1 special character (!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~).')
+    hashword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     # update.add_user(email, hashword)
-    return None
+    return True
 
 
 def valid_secret(secret: str):
@@ -35,7 +49,22 @@ def valid_secret(secret: str):
     :param secret: The supplied secret
     :return: **bool** Whether the secret meets the requirements.
     """
-    if re.fullmatch(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!\"#$%&'()*+,-./:;<=>?@\[\]^"
-                    r"_`{|}~]).{12,}", secret):
+    return re.fullmatch(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!\"#$%&'()*+,-./:;<=>?@\[\]^"
+                        r"_`{|}~]).{12,}", secret)
+
+
+def login(email: str, password: str):
+    """
+    Logs a user into the system, initializing a session.
+
+    :param email: **str** The email of the login request.
+    :param password: **str** An unhashed password that should match the account its attached to.
+    :return: **str** Returns a string indicating an error or **None** if login was successful.
+    """
+    if not query.is_user(email):
+        raise LoginError('This email is not attached to an account.')
+    user_id = query.get_user(email).id
+    if bcrypt.checkpw(password.encode('utf-8'), query.get_hashword(user_id)):
         return True
     return False
+
