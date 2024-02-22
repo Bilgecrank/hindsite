64cd72ae4e0508bb8f70dc502ea28433f83fa09c
Bilgecrank <8432868+Bilgecrank@users.noreply.github.com>
2024-02-22 14:21:59 -0800

    Expanded on core_models.py to handle most updating.

--- app/hindsite/core/core_model.py
+++ app/hindsite/core/core_model.py
@@ -71,18 +71,18 @@ def add_field(board: Board, name: str):
     return new_field
 
 
-def add_card(field: Field, author, card_body: str):
+def add_card(field: Field, author: 'User', card_message: str):
     """
     Adds a new card to a field.
 
     :param field: **Field** Target field the card is being added to.
     :param author: **User** User object that created the card.
-    :param card_body: **str** The initial message the card will read.
+    :param card_message: **str** The initial message the card will read.
     :return: **Card** The card object committed in the database.
     """
-    if len(card_body) > 2000:
+    if len(card_message) > 2000:
         raise CardError("Card message is too long(2000 characters).")
-    new_card = Card(field, author, card_body)
+    new_card = Card(field, author, card_message)
     db.session.commit()
     return new_card
 
@@ -92,8 +92,8 @@ def set_end_date_for_board(board: Board, end_date_time: datetime.datetime):
     Sets a new end date for the board.
 
     :param board: **Board** The board to be modified.
-    :param end_date_time:
-    :return:
+    :param end_date_time: **datetime** The end date-time of the board.
+    :return: **Board** The board that was modified.
     """
     if end_date_time > board.start_time:
         raise BoardError("End time is before start time.")
@@ -102,7 +102,7 @@ def set_end_date_for_board(board: Board, end_date_time: datetime.datetime):
     return board
 
 
-def update_field(field: Field, name: str):
+def update_field_name(field: Field, name: str):
     """
     Updates the name of a field.
 
@@ -115,3 +115,59 @@ def update_field(field: Field, name: str):
     field.name = name
     db.session.commit()
     return field
+
+
+def move_card(card: Card, field: Field):
+    """
+    Changes the assigned field of a card from the current to the one specified.
+
+    :param card: **Card** The card to be moved.
+    :param field: **Field** The field the card will be moved to.
+    :return: **Card** The card that was moved.
+    """
+    card.field = field
+    db.session.commit()
+    return card
+
+
+def update_card_message(card: Card, card_message: str):
+    """
+    Updates the message on the card to a new value.
+
+    :param card: **Card** The card to be updated.
+    :param card_message: **str** The new or edited message.
+    :return: **Card** The updated card.
+    """
+    if len(card_message) > 2000:
+        raise CardError("Card message is too long(2000 characters).")
+    card.message_body = card_message
+    db.session.commit()
+    return card
+
+
+def update_card_owner(card: Card, new_owner: 'User'):
+    """
+    Updates the owner of a card indicating possession of an issue or retrospective.
+
+    :param card: **Card** The card to be updated.
+    :param new_owner: **User** The user being assigned to the card.
+    :return: **Card** The card that was updated.
+    """
+    card.owner = new_owner
+    db.session.commit()
+    return card
+
+
+def update_card_status(card: Card, new_status: str):
+    """
+    Changes the status of a card.
+
+    :param card: **Card** The card to be updated.
+    :param new_status: **str** The new status being assigned to the card.
+    :return: **Card** The card that was updated.
+    """
+    if len(new_status) > 50:
+        raise CardError("The status message is too long(50 characters).")
+    card.card_status = new_status
+    db.session.commit()
+    return card
