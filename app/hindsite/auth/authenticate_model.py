690c1f4ffb25abb9606f6e0112acfc2af1722b95
Vincent Testagrossa <lancevancevt@gmail.com>
2024-02-23 17:13:51 -0500

    pylint fix (line too long)

--- app/hindsite/auth/authenticate_model.py
+++ app/hindsite/auth/authenticate_model.py
@@ -118,8 +118,9 @@ def valid_email(email: str):
 
     :returns: **bool** Whether the email meats the requirements or not.
     """
-    return re.fullmatch(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)"
-                        r"*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+    return re.fullmatch(
+        r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)"
+        r"*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
                         email)
 
 
