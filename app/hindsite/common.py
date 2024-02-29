1e99292b5774feadfda049f7997bb7b882b49f4c
Bilgecrank <8432868+Bilgecrank@users.noreply.github.com>
2024-02-29 14:04:53 -0800

    Set up dynamic updates for invites and retrospectives on notification.

--- app/hindsite/common.py
+++ app/hindsite/common.py
@@ -3,8 +3,13 @@ Template route testing for base page.
 """
 
 import os
-from flask import Blueprint, render_template, make_response
-from flask_login import login_required
+from datetime import datetime
+
+from flask import Blueprint, render_template, make_response, session
+from flask_login import login_required, current_user
+
+from app.hindsite.common_model import get_boards
+from app.hindsite.group.group_model import get_invitations
 
 static_dir = os.path.abspath('static')
 common = Blueprint('common',
@@ -20,7 +25,11 @@ def bubble():
     Updates the notification badge on the menu button.
     :return:
     """
-    count = 1
+
+    count = get_num_of_invites()
+    # Only return one for active retrospective.
+    if get_retro_active() > 0:
+        count += 1
     return render_template("partials/bubble.html", count=count)
 
 
@@ -31,7 +40,7 @@ def retro_active():
     Returns whether a retrospective is active for the current group.
     :return:
     """
-    count = 1
+    count = get_retro_active()
     return render_template("partials/retro_active.html", count=count)
 
 
@@ -42,18 +51,37 @@ def invite_count():
     Returns a number current invites.
     :return:
     """
-    count = 1
+    count = get_num_of_invites()
     return render_template("partials/invite_count.html", count=count)
 
 
-def get_retro_active(group_id: int):
+def get_retro_active():
     """
+    Checks for any active retrospectives.
 
     :return: **int**
     """
+    retro_count = 0
+    if 'groupid' not in session \
+            or session['groupid'] is None \
+            or session['groupid'] == 'Select a Group':
+        # User has not selected a group..
+        return 0
+    boards = get_boards(session['groupid'])
+    for board in boards:
+        if datetime.now() > board.start_time:
+            if board.end_time is not None:
+                if datetime.now() < board.end_time:
+                    retro_count += 1
+            else:
+                retro_count += 1
     return retro_count
 
 
 def get_num_of_invites():
+    """
+    Returns the number of active invites attached to the cou
 
-    return invite_count
+    :return:
+    """
+    return len(get_invitations(current_user.id))
