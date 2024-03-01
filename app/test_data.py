28285fda38b356b984dfb206b33e6c9bde0ce7cf
Bilgecrank <8432868+Bilgecrank@users.noreply.github.com>
2024-03-01 11:17:15 -0800

    Made end-dates for boards non-optional, and added a method in  to return a list of boards by end-date descending.

--- app/test_data.py
+++ app/test_data.py
@@ -4,7 +4,7 @@ Test data function to populate the table with tables.
 from datetime import datetime
 
 from app.hindsite.auth.authenticate_model import register_user
-import app.hindsite.core.core_model as core
+import app.hindsite.common_model as common
 from app.hindsite.common_model import get_user
 
 from app.hindsite.home.home_model import create_group
@@ -18,11 +18,12 @@ def populate_database(app):
     :param app: Running flask app.
     """
     with app.app_context():
-        # db.drop_all()
+        db.drop_all()
         db.create_all()
-        # group = create_test_user_and_group()
-        # simulate_invite(group)
-        # create_test_bfcs(group)
+        group = create_test_user_and_group()
+        simulate_invite(group)
+        create_test_bfcs(group)
+        print(common.get_all_boards_by_end_date(group.id))
         # tests_for_bfcs(group)
         # toggle_bfcs(group)
         # show_active_and_inactive_bfcs(group)
@@ -75,17 +76,17 @@ def create_test_bfcs(group):
     :param group:
     :return:
     """
-    board = core.create_board(group.id)
-    core.create_board(group.id)
-    core.create_board(group.id)
-    field = core.add_field(board, 'A really good field.')
-    core.add_field(board, 'A really good field.')
-    core.add_field(board, 'A really good field.')
-    core.add_card(field, get_user('emperorsgoodboi@imperium.vox'), 'Test '
+    board = common.create_board(group.id)
+    common.create_board(group.id)
+    common.create_board(group.id)
+    field = common.add_field(board, 'A really good field.')
+    common.add_field(board, 'A really good field.')
+    common.add_field(board, 'A really good field.')
+    common.add_card(field, get_user('emperorsgoodboi@imperium.vox'), 'Test '
                                                                    'Test Test')
-    core.add_card(field, get_user('emperorsgoodboi@imperium.vox'), 'Test '
+    common.add_card(field, get_user('emperorsgoodboi@imperium.vox'), 'Test '
                                                                    'Test Test Test')
-    core.add_card(field, get_user('emperorsgoodboi@imperium.vox'), 'Test ')
+    common.add_card(field, get_user('emperorsgoodboi@imperium.vox'), 'Test ')
 
 
 def tests_for_bfcs(group):
@@ -100,11 +101,11 @@ def tests_for_bfcs(group):
     field = board.fields[1]  # Second field
     card = board.fields[0].cards[1]  # Second card of first field.
     print(str(board.end_time))
-    core.set_end_date_for_board(board, datetime.now())
+    common.set_end_date_for_board(board, datetime.now())
     print(str(board.end_time))
     output = ''
-    core.move_card(card, field)
-    for a_card in core.get_cards(field):
+    common.move_card(card, field)
+    for a_card in common.get_cards(field):
         output = output + a_card.message_body
     print(output)
 
@@ -117,12 +118,12 @@ def toggle_bfcs(group):
     :param field:
     :return:
     """
-    boards = core.get_boards(group.id)
-    fields = core.get_fields(boards[0])
-    cards = core.get_cards(fields[0])
-    core.toggle_archive_board(boards[2])
-    core.toggle_archive_field(fields[2])
-    core.toggle_archive_card(cards[1])
+    boards = common.get_boards(group.id)
+    fields = common.get_fields(boards[0])
+    cards = common.get_cards(fields[0])
+    common.toggle_archive_board(boards[2])
+    common.toggle_archive_field(fields[2])
+    common.toggle_archive_card(cards[1])
 
 
 def show_active_and_inactive_bfcs(group):
@@ -135,23 +136,23 @@ def show_active_and_inactive_bfcs(group):
     """
     output = ''
     print('Active Entities')
-    for got_board in core.get_boards(group.id):
+    for got_board in common.get_boards(group.id):
         output = output + str(got_board.id) + ' | '
     output = output + '\n'
-    for got_field in core.get_fields(group.boards[0]):
+    for got_field in common.get_fields(group.boards[0]):
         output = output + got_field.name + ' | '
     output = output + '\n'
-    for got_card in core.get_cards(group.boards[0].fields[0]):
+    for got_card in common.get_cards(group.boards[0].fields[0]):
         output = output + got_card.message_body + ' | '
     print(output)
     print('Inactive Entities')
     output = ''
-    for got_board in core.get_boards(group.id, True):
+    for got_board in common.get_boards(group.id, True):
         output = output + str(got_board.id) + ' | '
     output = output + '\n'
-    for got_field in core.get_fields(group.boards[0], True):
+    for got_field in common.get_fields(group.boards[0], True):
         output = output + got_field.name + ' | '
     output = output + '\n'
-    for got_card in core.get_cards(group.boards[0].fields[0], True):
+    for got_card in common.get_cards(group.boards[0].fields[0], True):
         output = output + got_card.message_body + ' | '
     print(output)
