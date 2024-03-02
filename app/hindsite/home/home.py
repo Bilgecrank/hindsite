c2b90eab81585c43eaf07ed5a51b1e9dfee7ea2c
Vincent Testagrossa <lancevancevt@gmail.com>
2024-03-02 11:05:17 -0500

    reorganized facilitator and participant views. Most of the logic is in the homepage route now, with each view only rendering the specific route

--- app/hindsite/home/home.py
+++ app/hindsite/home/home.py
@@ -41,21 +41,9 @@ def homepage():
         session['groupname'] = "Select a Group"
     selected = session['groupname']
 
-    #TODO: The first version below is the correct route. Uncomment it to enable 
-    # proper routing.
-    #
-    # The second version below is to test/develop facilitator_route()
-
-    return authorized(facilitator_route(selected), participant_route(selected))
-    # return authorized(participant_route(selected), facilitator_route(selected)) #TODO: TESTING
-
-def participant_route(selected: str):
-    """
-        Loads home.html, sets the title and loads in the group
-        selection dropdown. Periodically checks for invites sent
-        from other users to their groups and loads them.
-    """
     groups = get_groups(current_user.id)
+
+    # Need to check for 'POST' first in case there's a redirect from a participant
     if request.method == 'POST':
         try:
             session['groupname'] = request.args['groupname']
@@ -63,23 +51,13 @@ def participant_route(selected: str):
             group_id = session.get('groupid')
             ownership = get_ownership(current_user.id, group_id)
             session['facilitator'] = ownership
-        except GroupAddError as ex:
-            flash(ex.message)
+        except Exception as ex:
+            print(ex)
         if 'groupname' in session:
             selected = session['groupname']
-        return render_template('home.html', title='Home', \
-                               groups=groups, selected=selected)
-    
-    return render_template('home.html', title='Home', groups=groups, selected=selected)
 
-def facilitator_route(selected: str):
-    """
-        Route for Facilitator mode
-    """
-    groups = get_groups(current_user.id)
     board = None
     boards = None
-    # TODO: Get the boards for the group
     if 'groupid' in session and session['groupid'] is not None:
         # a group is selected, so we can populate the boards
         group_id = session.get('groupid')
@@ -90,39 +68,27 @@ def facilitator_route(selected: str):
             board = create_board(group_id)
             field = add_field(board, "New Category")
             add_card(field, get_user(current_user.id), 'Enter Card Data')
-        else:
-            #populates the board selector
-            #selects the most recent board
+        boards = get_boards(group_id)
+        #populates the board selector
+        #selects the most recent board
+        #TODO: add a selection method for the board
+        board = boards[0]
+    return authorized(facilitator_route(selected, groups, board), participant_route(selected, groups, board))
 
-            #TODO: add a selection method for the board
-            board = boards[0]
+def participant_route(selected: str, groups: Group, board: Board):
+    """
+        Loads home.html, sets the title and loads in the group
+        selection dropdown. Periodically checks for invites sent
+        from other users to their groups and loads them.
+    """
+    return render_template('home.html', title='Home', groups=groups, selected=selected, board=board)
 
-    if request.method == 'POST':
-        try:
-            session['groupname'] = request.args['groupname']
-            session['groupid'] = int(request.args['group_id'])
-        except GroupAddError as ex:
-            flash(ex.message)
-        if 'groupname' in session:
-            selected = session['groupname']
-        return render_template('facilitator-home.html', title='Facilitator Home', \
-                               groups=groups, selected=selected, board=board)
+def facilitator_route(selected: str, groups: Group, board: Board):
+    """
+        Route for Facilitator mode
+    """
     return render_template('facilitator-home.html', title='Facilitator Home', \
                            groups=groups, selected=selected, board=board)
-
-@home.route('/facilitator-display', methods=['GET', 'POST'])
-@login_required
-def facilitator_display():
-    """
-        Route to retrieve the cards using HTMx polling
-    """
-    groupid = ''
-    board = None
-    if session.get('groupid') is not None:
-        groupid = session.get('groupid')
-        boards = get_boards(groupid, False)
-        board = boards[0]
-    return render_template('partials/facilitator-blob.html', title='Home', board=board)
 
 
 # EDIT ROUTES AND POST ROUTES
